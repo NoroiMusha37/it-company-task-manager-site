abb7b46e482945d720c2c1e7a86cf89879b75c95
NoroiMusha37 <vo.melnyk37@gmail.com>
2025-10-08 23:27:59 +0300

    add routes for all list views

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,33 @@
 from django.urls import path
 from .views import (
     index,
+    TaskTypeListView,
+    PositionListView,
+    WorkerListView,
+    TaskListView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "task-types/",
+        TaskTypeListView.as_view(),
+        name="task-type-list",
+    ),
+    path(
+        "positions/",
+        PositionListView.as_view(),
+        name="position-list",
+    ),
+    path(
+        "workers/",
+        WorkerListView.as_view(),
+        name="worker-list",
+    ),
+    path(
+        "tasks/",
+        TaskListView.as_view(),
+        name="task-list",
+    ),
+
 ]
