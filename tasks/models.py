99d3181c57be8e79825cf6841c053435af792955
NoroiMusha37 <vo.melnyk37@gmail.com>
2025-10-08 21:21:37 +0300

    add TaskType, Position, Worker, and Task models
    TaskType & Position have only 'name' attribute
    Worker inherits from AbstractUser and has 'position' FK
    Task has 'name', 'description', 'deadline', 'is_completed', 'priority' (multi-choice)
    Task has FK 'task_type' -> TaskType and ManyToMany 'assignees' -> Worker

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import User, AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+
+class Task(models.Model):
+    PRIORITIES = [
+        ("P0", "Critical"),
+        ("P1", "High"),
+        ("P2", "Moderate"),
+        ("P3", "Low"),
+        ("P4", "Negligible"),
+    ]
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=1, choices=PRIORITIES)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker)
