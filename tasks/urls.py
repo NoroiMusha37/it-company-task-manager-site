59b7bd9f9f7e3b5040c77e40adee360f45955453
NoroiMusha37 <vo.melnyk37@gmail.com>
2025-10-10 22:40:44 +0300

    add creation routes for all models in tasks/urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,6 +7,10 @@ from .views import (
     TaskListView,
     WorkerDetailView,
     TaskDetailView,
+    TaskTypeCreateView,
+    PositionCreateView,
+    WorkerCreateView,
+    TaskCreateView,
 )
 
 urlpatterns = [
@@ -17,9 +21,19 @@ urlpatterns = [
         name="task-type-list",
     ),
     path(
+        "task-types/create/",
+        TaskTypeCreateView.as_view(),
+        name="task-type-create"
+    ),
+    path(
         "positions/",
         PositionListView.as_view(),
         name="position-list",
+    ),
+    path(
+        "positions/create/",
+        PositionCreateView.as_view(),
+        name="position-create"
     ),
     path(
         "workers/",
@@ -27,12 +41,26 @@ urlpatterns = [
         name="worker-list",
     ),
     path(
+        "workers/<int:pk>/",
+        WorkerDetailView.as_view(),
+        name="worker-detail"
+    ),
+    path(
+        "workers/create/",
+        WorkerCreateView.as_view(),
+        name="worker-create"
+    ),
+    path(
         "tasks/",
         TaskListView.as_view(),
         name="task-list",
     ),
-    path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path(
+        "tasks/create/",
+        TaskCreateView.as_view(),
+        name="task-create"
+    ),
 ]
 
 app_name = "tasks"
