589fcb3a9f3d4019ed4f7bd3fde057e1d6a2d036
NoroiMusha37 <vo.melnyk37@gmail.com>
2025-10-22 19:30:25 +0300

    move MIDDLEWARE from base.py to dev.py and prod.py

--- it_company_task_manager/settings/dev.py
+++ it_company_task_manager/settings/dev.py
@@ -7,6 +7,18 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
